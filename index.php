30ca1f2910e50908a0aa5528a6906541a19949a0
Teddy <r2d2f9@gmail.com>
2016-03-29 05:56:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -78,7 +78,6 @@ if (isset($_GET['l'])) {
 		$leng = 'en';
 	}
 //index.php for localhost
-// date date ("F d Y H:i:s.", filemtime($path))
 $d = array();
 $p = array();
 
@@ -93,33 +92,14 @@ foreach (glob("{pr/*/*/*.js,pr/*/*/*.php,pr/*/*/*.css,pr/*/*/*.txt,pr/*/*/*.png}
 foreach (glob("{pr/*/*.js,pr/*/*.php,pr/*/*.css,pr/*/*.txt,pr/*/*.png}",GLOB_BRACE) as $path) {//смотрим все папки в /pr
 		$name = substr($path, 6, -4);
 		$date = filemtime($path);
-		//echo '<a href="'.$path.'"><div class="block" text=""><h3>'.$path.'</h3><p text="">'.$date.'<p></div></a>';
-		array_push($d,strval($date));//заносим дату каждой в массив;
-		$p[$date] = $path;//заносим их путь с ключом-временем
-		//echo $path.'<br>';
-}
-//foreach (glob("pr/*/*") as $path) {//смотрим все папки в /pr
-/*		$name = substr($path, 6, -4);
-		$date = filemtime($path);
-		//echo '<a href="'.$path.'"><div class="block" text=""><h3>'.$path.'</h3><p text="">'.$date.'<p></div></a>';
 		array_push($d,strval($date));//заносим дату каждой в массив;
 		$p[$date] = $path;//заносим их путь с ключом-временем
 }
-foreach (glob("pr/*") as $path) {//смотрим все папки в /pr
-		$name = substr($path, 6, -4);
-		$date = filemtime($path);
-		//echo '<a href="'.$path.'"><div class="block" text=""><h3>'.$path.'</h3><p text="">'.$date.'<p></div></a>';
-		array_push($d,strval($date));//заносим дату каждой в массив;
-		$p[$date] = $path;//заносим их путь с ключом-временем
-}*/
 rsort($d);//сортируем массив с датами по убыванию
 $n = 0;//начинаем отсчет файлов внутри каждой папки
 foreach ($d as $num) {
-	//echo $p[$num].' - '.date ("F d Y H:i:s.", filemtime($p[$num])).'<br>';
-	//echo substr($p[$num], 0, 5).'<br>';
 	if($n < 1){//выбираем только первый файл/папку
 		$path = substr($p[$num], 0, 5);
-		//echo	'<a href="'.$p[$num].'">'.$p[$num].'<a><br>';/*
 		$d1 = array();//массив для времени
 		$p1 = array();//массив для путей
 		$l = 0;//начинаем отсчет файлов внутри выбраной папки в /пр
@@ -127,38 +107,21 @@ foreach ($d as $num) {
 			$l++;
 			$name1 = substr($path1, 6, -4);
 			$date1 = filemtime($path1);
-			//echo '<a href="'.$path.'"><div class="block" text=""><h3>'.$path.'</h3><p text="">'.$date.'<p></div></a>';
 			array_push($d1,strval($date1));//вносим дату в массив 2 уровня
 			$p1[$date1] = $path1;//вносим путь в массив 2 уровня для путей
 			//echo $l;
 		}
-		/*if($l>0){
-			
-			arsort($d1);
-			$n1 = 0;
-			foreach ($d1 as $num1) {
-				if($n1<1){*/
-					$readme = substr($p[$num],0,5).'/readme.txt';
-					//echo intval('041')." ";
-					//echo html_entity_decode(file_get_contents($readme, NULL, NULL, 0, 3));
-					//<img class="img" src="'.substr($p[$num],0,5).'/img/logo.png">
-					//<img class="lastdiv" src="'.substr($p[$num],0,5).'/img/cover.png">
-					$n1lr = intval(file_get_contents($readme, NULL, NULL, 0, 3));
-				 	$n1l = intval(file_get_contents($readme, NULL, NULL, 4, 6));
-					echo'<div class="lastcont">';
-					if($leng == 'ru'){
-						echo '<span class="span">Последнее изменение в:</span><br><a href="'.substr($p[$num],0,5).'" target="_parent">'.file_get_contents($readme, NULL, NULL, 6,$n1lr).'</a><br><span class="lasttime">'.date ("F d Y H:i:s.", filemtime($p[$num])).'</span>';
-					}else{
-						echo	'<span class="span">Last modification in:</span><br><a href="'.substr($p[$num],0,5).'" target="_parent">'.file_get_contents($readme, NULL, NULL, 6+$n1lr, $n1l).'</a><br><span class="lasttime">'.date ("F d Y H:i:s.", filemtime($p[$num])).'</span>';
-					}
-					echo '</div>';
-					//echo	'<ul><a href="'.$p1[$num].'">'.$p1[$num].'<a><br></ul>';
-				/*}
-				$n1++;
-			}
+
+		$readme = substr($p[$num],0,5).'/readme.txt';
+		$n1lr = intval(file_get_contents($readme, NULL, NULL, 0, 3));
+		$n1l = intval(file_get_contents($readme, NULL, NULL, 4, 6));
+		echo'<div class="lastcont">';
+		if($leng == 'ru'){
+			echo '<span class="span">Последнее изменение в:</span><br><a href="'.substr($p[$num],0,5).'" target="_parent">'.file_get_contents($readme, NULL, NULL, 6,$n1lr).'</a><br><span class="lasttime">'.date ("F d Y H:i:s.", filemtime($p[$num])).'</span>';
 		}else{
-			$n--;
-		}*/
+			echo	'<span class="span">Last modification in:</span><br><a href="'.substr($p[$num],0,5).'" target="_parent">'.file_get_contents($readme, NULL, NULL, 6+$n1lr, $n1l).'</a><br><span class="lasttime">'.date ("F d Y H:i:s.", filemtime($p[$num])).'</span>';
+		}
+		echo '</div>';
 	}
 	$n++;
 }
